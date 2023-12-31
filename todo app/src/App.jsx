ca08f8ae449010896aa0c76e1530c7e48b3f51cc
unknown <oraza7969@gmail.com>
2023-12-31 06:09:59 -0800

    React todo app

--- todo app/src/App.jsx
+++ todo app/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
 
 function App() {
@@ -12,25 +11,52 @@ function App() {
 function addTodo(event) {
   event.preventDefault();
   console.log(text);
-  todo.push(text)
-  setTodo([...todo]);
-  console.log(todo);
-  setText ("")
+  if (text !== "") {
+    todo.push(text)
+    setTodo([...todo]);
+    console.log(todo);
+    setText ("")
+  }else{
+    alert('Please enter something')
+  }
 }
 
+//function delete Todo
+
+function deleteTodo(index) {
+  const deleteTodo = [...todo];
+  deleteTodo.splice(index, 1)
+  setTodo(deleteTodo)
+}
+
+
+//function Edit Todo
+
+function updateTodo(index) {
+  const updatedText = prompt('Update Todo', todo[index]);
+  if (updatedText !== '') {
+    const updatedTodos = [...todo];
+    updatedTodos[index] = updatedText;
+    setTodo(updatedTodos)
+  }else{
+    alert('Please enter something')
+  }
+}
   return (
     <>
-      <h2>Todo App {text}</h2>
+      <h2>Todo App </h2>
+      <div className="container">
       <form onSubmit={addTodo}>
         <input type="text" placeholder='Enter your text' onChange={(e)=>setText(e.target.value)} value={text}/>
-        <button type='submit'>Add</button>
+        <button className='add-btn' type='submit'>Add</button>
       </form>
+      <hr />
+      </div>
       { <ul>
         {todo.map((item, index)=>{
           return <li key={index}>{item}
-          <button onClick={()=>deleteTodo(index)}>delete</button><span> </span>
-          <button onClick={()=>editTodo(index)}>edit</button><br></br><hr></hr>
-            
+          <button className='dlt-btn' onClick={()=>deleteTodo(index)}>delete</button><span> </span>
+          <button className='edit-btn' onClick={()=>updateTodo(index)}>edit</button><br></br><hr></hr>
           </li>
         })}
       </ul> }
